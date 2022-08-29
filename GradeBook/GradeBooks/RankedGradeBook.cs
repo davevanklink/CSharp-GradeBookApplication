fc21cb2b3bb0b217e36406b64548d93bb92c3fb7
Dave van Klink <dave.vanklink@iddinkgroup.com>
2022-08-29 12:59:33 +0200

    a

--- GradeBook/GradeBooks/RankedGradeBook.cs
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -36,10 +36,20 @@ namespace GradeBook.GradeBooks
         {
             if (Students.Count() < 5)
             {
-                Console.WriteLine("Ranked grading requires at least 5 students with grades in order to properly calculate a student's overall grade.");
+                Console.WriteLine("Ranked-grading requires a minimum of 5 students to work");
                 return;
             }
             base.CalculateStatistics();
+        }
+
+        public override void CalculateStudentStatistics(string name)
+        {
+            if (Students.Count() < 5)
+            {
+                Console.WriteLine("Ranked-grading requires a minimum of 5 students to work");
+            }
+
+            base.CalculateStudentStatistics(name);
         }
     }
 }
